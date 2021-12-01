6e9be9c62600d1ee4ca6cc1258874bc285996d86
AlexGarciaG <alegg-1998@outlook.com>
2021-12-01 10:27:32 -0600

    Configure changes

--- src/object_detection.cpp
+++ src/object_detection.cpp
@@ -5,6 +5,7 @@
 #include <sensor_msgs/image_encodings.h>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include "utils.h"
 
 
 #include <dynamic_reconfigure/server.h>
@@ -45,6 +46,22 @@ public:
     int varerosion8=1;
     int varopening8=0;
     int varclosing8=0;
+
+    long double msdilation16=0;
+    long double mserosion16=2;
+    long double msopening16=0;
+    long double msclosing16=0;
+    long double msThreshold16=1000;
+    long double ms16=1000;
+    //8 bits
+    long double msdilation8=0;
+    long double mserosion8=1;
+    long double msopening8=0;
+    long double msclosing8=0;
+    long double msCounter=0;
+    long double ms8=1000;
+
+
   ImageConverter(): it_(nh_)
   {
     // Subscrive to input video feed and publish output video feed
@@ -399,23 +416,35 @@ public:
      * Cleans depth image and makes edge detection
      * *********************************/
     //Execute dilation16 n times
+
+    start_timer();
     for(int i = 0;i<vardilation16;i++){
       dilation16();
     }
+    msdilation16=msdilation16+stop_timer();
+    start_timer();
     //Execute erosion16 n times
     for(int i = 0;i<varerosion16;i++){
       erosion16();
-    }   
+    }
+    mserosion16=mserosion16+stop_timer();
+    start_timer();
     //Execute opening16 n times
     for(int i = 0;i<varopening16;i++){
       opening16();
-    }  
+    }
+    msopening16=msopening16+stop_timer();
+    start_timer();
     //Execute closing16 n times
     for(int i = 0;i<varclosing16;i++){
       closing16();
     }
+    msclosing16=msclosing16+stop_timer();
+    start_timer();
     //Execute edge detection by a Threshold
     edge16();
+    msThreshold16=msThreshold16+stop_timer();
+    ms16= msdilation16+mserosion16+msopening16+msclosing16+msThreshold16;
   }
 
 
@@ -428,22 +457,30 @@ public:
      * Cleans edge image and detecs obstacles
      * *********************************/
     //Execute dilation8 n times
+    start_timer();
     for(int i = 0;i<vardilation8;i++){
       dilation8();
     }
-
+    msdilation8=msdilation8+stop_timer();
+    start_timer();
     //Execute opening8 n times
     for(int i = 0;i<varopening8;i++){
       opening8();
     }  
+    msopening8=msopening8+stop_timer();
+    start_timer();
     //Execute closing8 n times
     for(int i = 0;i<varclosing8;i++){
       closing8();
     }
+    msclosing8=msclosing8+stop_timer();
+    start_timer();
         //Execute erosion8 n times
     for(int i = 0;i<varerosion8;i++){
       erosion8();
     }   
+    mserosion8=mserosion8+stop_timer();
+    ms8 = msdilation8+msopening8+msclosing8+mserosion8;
   }
 
   void imageCb(const sensor_msgs::ImageConstPtr& msg)
@@ -465,28 +502,26 @@ public:
       ROS_ERROR("cv_bridge exception: %s", e.what());
       return;
     }
-    //Get the param fron the launcher
-    //16 bits
 
-    /*
-    //16 bits
-    ros::param::get("/dilation16",  vardilation16);
-    ros::param::get("/erosion16",   varerosion16);
-    ros::param::get("/opening16",   varopening16);
-    ros::param::get("/closing16",   varclosing16);
-    ros::param::get("/threshold16", varThreshold16);
-    //8 bits
-    ros::param::get("/dilation8",  vardilation8);
-    ros::param::get("/erosion8",   varerosion8);
-    ros::param::get("/opening8",   varopening8);
-    ros::param::get("/closing8",   varclosing8);
-    */
+
     //Make image procesing for depth image
     //16 bits
     imageProcesingForDepthImage();
     //o bits
     imageProcesingForEdgeImage();
-    
+    // print times 
+    msCounter++;
+    ROS_INFO("Total16= %Lf:Dilation16=%Lf,Erosion16=%Lf,Opening16==%Lf,Closing16=%Lf,Threshold16=%Lf",(ms16/msCounter),(msdilation16/msCounter),(mserosion16/msCounter),(msopening16/msCounter),(msclosing16/msCounter),(msThreshold16/msCounter));
+    ROS_INFO("Total8= %Lf:Dilation8=%Lf,Erosion8=%Lf,Opening8==%Lf,Closing8=%Lf",(ms8/msCounter),(msdilation8/msCounter),(mserosion8/msCounter),(msopening8/msCounter),(msclosing8/msCounter));
+    ROS_INFO("Total=%Lf",((ms8+ms16)/msCounter));
+    //8 bits
+   // long double msdilation8=0;
+   // long double mserosion8=1;
+   // long double msopening8=0;
+   // long double msclosing8=0;
+   // long double ms8=1000;
+
+
     // Update GUI Window
     cv::imshow(OPENCV_WINDOW, this->imageEdge);
     cv::waitKey(3);
